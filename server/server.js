03cafb3cb344faf41d195eef3cc1ce9e16d3b5b0
Jessica Fields <Jessica Fields>
2017-02-18 15:24:20 -0500

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash')
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -97,7 +99,7 @@ app.patch('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({todo});
+    res.send({todo})
   }).catch((e) => {
     res.status(400).send()
   })
